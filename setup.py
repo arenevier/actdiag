aeaa7f1fcf11da6a6ff827c1bfca512dbc8e5132
Arno Renevier <arno@renevier.net>
2024-07-20 18:54:15 -0700

    switch to pytest
    And use maintained blockdiag repo for test deps

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
             'docutils',
         ],
         testing=[
-            'nose',
+            'pytest',
             'pep8 >=1.3',
             'reportlab',
             'docutils',
